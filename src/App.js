507fb1d48c8f9c3ef3e98a365d7944aad9ec74dd
chinni00 <chinni0437@gmail>
2023-06-20 22:50:29 +0530

    when user try to access change password it doesnt allow

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Navigate } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const authCtx = useContext(AuthContext)
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<HomePage />} exact />
           
        
-        <Route path='/auth' element={<AuthPage />} />
+       {!authCtx.isLogedIn && <Route path='/auth' element={<AuthPage />} />}
           
-        <Route path='/profile' element={<UserProfile />} />
-          
+       {authCtx.isLogedIn && <Route path='/profile' element={<UserProfile />} />}
+          <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </Layout>
   );
